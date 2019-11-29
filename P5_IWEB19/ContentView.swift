340f910e46dc75d5212de5257cc20bd9d1ebba56
Nestor Martinez <nestor.martinez@alumnos.upm.es>
2019-11-29 19:35:23 +0100

    Initial Commit

---
+++ P5_IWEB19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P5_IWEB19
+//
+//  Created by g950 DIT UPM on 29/11/2019.
+//  Copyright © 2019 g950 DIT UPM. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
